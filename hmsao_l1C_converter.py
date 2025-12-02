c479f1f0d4a91d42e1c72935f97c213ec00e5f7b
Charmi Patel <charmi.patel1198@gmail.com>
2025-12-02 17:42:47 -0500

    Restructured

--- hmsao_l1C_converter.py
+++ hmsao_l1C_converter.py
@@ -1,88 +1,102 @@
-#convert level 1B -> 1C
-#%%
-#%%
+# convert level 1B -> 1C
+# %%
+# %%
 from datetime import datetime
 import numpy as np
 import xarray as xr
-from skimage import transform
 from pathlib import Path
-from secondary_straightening import secondary_straightening
-from matplotlib import pyplot as plt
-from tqdm import tqdm
+# %%
+LOCALPATH = Path(__file__).parent
+CALIBMAP_DIR = LOCALPATH / 'calib_maps'
+
+def main(
+    datdir: Path = Path('/home/charmi/locsststor/proc/hmsao/l1b'),
+    destdir: str | Path | None = '',
+    calibmap_dir: Path | None = CALIBMAP_DIR,
+    photon:  bool = True
+):
+    if destdir is None:
+        destd = Path('./l1c')
+    elif isinstance(destdir, str) and destdir == '':
+        destd = Path(str(datdir).replace('l1b', 'l1c'))
+    elif isinstance(destdir, str):
+        destd = Path(destdir)
+    else:
+        destd = destdir
+    destd.mkdir(exist_ok=True)
+    if calibmap_dir is None:  # if None, check current folder
+        cmap_dir = LOCALPATH / 'calib_maps'
+    else:
+        cmap_dir = calibmap_dir
+
+    calibfns = list(cmap_dir.glob(f'*calib*.nc'))
+    if len(calibfns) < 1:
+        raise FileNotFoundError(
+            f"No calibration map files found in {cmap_dir}."
+        )
+    windows = set([fp.stem.split('_')[-1] for fp in calibfns])
+    windows = sorted(list(windows))
+
+    for win in windows:
+        print(f"Processing window: {win}")
+        calibfns = list(cmap_dir.glob(f'*{win}*.nc'))
+        if len(calibfns) > 0:
+            print(
+                f"Multiple calibration map files found for window {win}. Using the first one: {calibfns[0].name}")
+            cfn = calibfns[0]
+
+        calibds = xr.open_dataset(cfn)  # type: ignore
+
+        print(f"Destination directory: {destd}")
+
+        fns = list(datdir.glob(f'**/*{win}*.nc'))
+        print(f"Found {len(fns)} files to process.")
+        fns.sort()
+
+        if photon:
+            print("Calibrating to photons/s.cm2.sr...")
+            id = 'kp'
+            units = 'photons/s.cm2.sr.nm'
+        else:
+            print("Calibrating to Rayleighs...")
+            id = 'kr'
+            units = 'Rayleighs/nm'
+
+        for fn in fns:
+            print(f"Processing file: {fn.name}...", end='', flush=True)
+            ds = xr.open_dataset(fn)
+            ss = ds.copy()
+            dwl = np.mean(np.diff(ds.wavelength.data))
+            ss.countrate.data = ss.countrate.data * calibds[id].data / dwl
+            ss.noise.data = ss.noise.data * calibds[id].data / dwl
+
+            all_vars = list(ds.coords) + list(ds.keys())
+            for var in all_vars:
+                attrs = {}
+                for k, v in ds[var].attrs.items():
+                    if k not in ['unit']:
+                        continue
+                    else:
+                        k = 'units'
+                    attrs[k] = v
+                ss[var].attrs = attrs
+
+            ss['countrate'].attrs.update({'units': units})
+            ss['countrate'].attrs.update({'long_name': 'Calibrated Intensity'})
+            ss = ss.rename_vars({'countrate': 'intensity'})
+            ss['noise'].attrs.update({'units': units})
+
+            attrs = {k: v for k, v in ds.attrs.items() if k not in 'unit'}
+            ss.attrs = attrs
+            ss.attrs['DataProcessingLevel'] = 'L1c - Calibrated.'
+            ss.attrs['FileCreationDate'] = datetime.now().strftime(
+                "%m/%d/%Y, %H:%M:%S EDT")
+            encoding = {var: {'zlib': True}
+                        for var in (*ss.data_vars.keys(), *ss.coords.keys())}
+            print('\tsaving...', end='', flush=True)
+            outfn = destd.joinpath(fn.stem.replace('l1b', 'l1c') + fn.suffix)
+            ss.to_netcdf(outfn, encoding=encoding)
+            print('\tDone.', flush=True)
+
 
 # %%
-######### user inputs #########
-windows = ['5577', '6300']
-datdir = Path('../data/l1b')
-destdir = '' #if empty string, will replace 'l1b' with 'l1c' in datdir path. If None, will use './l1c' as destdir.
-calibmap_dir = None #if empty string, will replace 'l1b' with 'calib_maps' in datdir path. If None, will use './calib_maps' as calibmap_dir.
-photon = False # if true, calibrate to photons/s.cm2.sr, if false, calibrate to Rayleighs
-##################################
-for win in windows:
-    print(f"Processing window: {win}")
-    if calibmap_dir is None: # if None, check current folder
-        calibmap_dir = Path('./')
-    elif calibmap_dir == '': # if emply, check in datadir for calib_maps
-        calibmap_dir = Path(str(datdir).replace('l1b','calib_maps'))
-    
-    calibfns = list(calibmap_dir.glob(f'*calib*{win}*.nc'))
-    if len(calibfns) < 1:
-        raise FileNotFoundError(f"No calibration map files found in {calibmap_dir} for window {win}.")
-    elif len(calibfns) > 0:
-        print(f"Multiple calibration map files found for window {win}. Using the first one: {calibfns[0].name}")
-        cfn = calibfns[0]
-
-    calibds = xr.open_dataset(cfn) #type: ignore
-
-    if destdir is None:
-        destdir = Path('./l1c')
-    elif destdir == '' :
-        destdir = Path(str(datdir).replace('l1b','l1c'))
-    destdir.mkdir(exist_ok=True)
-    print(f"Destination directory: {destdir}")
-    
-    fns = list(datdir.glob(f'**/*{win}*.nc'))
-    print(f"Found {len(fns)} files to process.")
-    fns.sort()
-    
-    if photon: 
-        print ("Calibrating to photons/s.cm2.sr...")
-        id = 'kp'
-        units = 'photons/s.cm2.sr.nm'
-    else: 
-        print ("Calibrating to Rayleighs...")
-        id = 'kr'
-        units = 'Rayleighs/nm'
-
-    for fn in fns:
-        print(f"Processing file: {fn.name}...")
-        ds = xr.open_dataset(fn)
-        ss = ds.copy()
-        dwl = np.mean(np.diff(ds.wavelength.data))
-        ss.countrate.data = ss.countrate.data * calibds[id].data / dwl
-        ss.noise.data = ss.noise.data * calibds[id].data / dwl
-
-        all_vars = list(ds.coords) + list(ds.keys())
-        for var in all_vars:
-            attrs = {}
-            for k,v in ds[var].attrs.items():
-                if k not in ['unit']:
-                    continue
-                else: k = 'units'
-                attrs[k] = v
-            ss[var].attrs = attrs
-
-        ss['countrate'].attrs.update({'units': units})
-        ss['countrate'].attrs.update({'long_name': 'Calibrated Intensity'}) 
-        ss = ss.rename_vars({'countrate':'intensity'})
-        ss['noise'].attrs.update({'units': units})
-        
-        attrs = {k:v for k,v in ds.attrs.items() if k not in 'unit'}
-        ss.attrs = attrs
-        ss.attrs['DataProcessingLevel'] = 'L1c - Calibrated.'
-        ss.attrs['FileCreationDate'] = datetime.now().strftime("%m/%d/%Y, %H:%M:%S EDT")
-        encoding = {var: {'zlib': True} for var in (*ss.data_vars.keys(), *ss.coords.keys())}
-        outfn = destdir.joinpath(fn.stem.replace('l1b','l1c') + fn.suffix)
-        ss.to_netcdf(outfn, encoding=encoding)
-        print(f"Saved L1C file: {outfn.name}")
-
