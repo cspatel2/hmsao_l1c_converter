d409174d5b9af951e4309b490627d88defa0be79
Charmi Patel <charmi.patel1198@gmail.com>
2026-03-13 22:32:32 +0000

    made compatile with pipeline.py

--- hmsao_l1C_converter.py
+++ hmsao_l1C_converter.py
@@ -1,47 +1,86 @@
 # convert level 1B -> 1C
 # %%
 # %%
+from dataclasses import dataclass
 from datetime import datetime
 import numpy as np
+import tqdm
+from tqdm import tqdm
 import xarray as xr
 from pathlib import Path
+import argparse
+    
 # %%
-LOCALPATH = Path(__file__).parent
-CALIBMAP_DIR = LOCALPATH / 'calib_maps'
+@dataclass
+class L1CConfig:
+    """ Configuration for L1C conversion.
+    Arguments:
+        rootdir (str|Path): Root directory containing L1B files.
+        destdir (str|Path): Destination directory for L1C files. If not provided, will create a 'l1c' directory parallel to the rootdir.
+        windows (list[str]): List of windows to process. If not provided, will process all windows found in the calibration map directory.
+        calibmap_dir (str|Path): Directory containing calibration map files.
+        photon (bool): If set, calibrate to photons/s.cm2.sr.nm. If not set, calibrate to Rayleighs/nm. Default is True.
+    """
+    rootdir: str | Path
+    destdir: str | Path
+    windows: list[str]
+    calibmap_dir: str | Path
+    photon: bool = True
+
+def main(config: L1CConfig):
+    """ Converts HMS data from L1b -> L1C.
+    uses calibration maps to convert from countrate to physical units (photons/s.cm2.sr.nm or Rayleighs/nm).
+
+    Args:
+        config (L1CConfig): Configuration for L1C conversion.
+            - rootdir (str|Path): Root directory containing L1B files.
+            - destdir (str|Path): Destination directory for L1C files. If not provided, will create a 'l1c' directory parallel to the rootdir.
+            - windows (list[str]): List of windows to process. If not provided, will process all windows found in the calibration map directory.
+            - calibmap_dir (str|Path): Directory containing calibration map files.
+            - photon (bool): If set, calibrate to photons/s.cm2.sr.nm. If not set, calibrate to Rayleighs/nm. Default is True.
 
 
+    Raises:
+        FileNotFoundError: If no calibration map files are found in the specified directory.
+    """    
+    #check Root dir
+    if isinstance(config.rootdir, str):
+        config.rootdir = Path(config.rootdir)
+    config.rootdir = config.rootdir.expanduser()
 
-def main(
-    datdir: Path = Path('/home/charmi/locsststor/proc/hmsao/l1b'),
-    destdir: str | Path | None = '',
-    calibmap_dir: Path | None = CALIBMAP_DIR,
-    photon:  bool = True
-):
-    if destdir is None:
-        destd = Path('./l1c')
-    elif isinstance(destdir, str) and destdir == '':
-        destd = Path(str(datdir).replace('l1b', 'l1c'))
-    elif isinstance(destdir, str):
-        destd = Path(destdir)
-    else:
-        destd = destdir
-    destd.mkdir(exist_ok=True)
-    if calibmap_dir is None:  # if None, check current folder
-        cmap_dir = LOCALPATH / 'calib_maps'
-    else:
-        cmap_dir = calibmap_dir
+    #check Destination Dir
+    if isinstance(config.destdir, str):
+        if config.destdir == '':
+            config.destdir = Path(str(config.rootdir).replace('l1b', 'l1c'))
+        else:
+            config.destdir = Path(config.destdir)
+    config.destdir = config.destdir.expanduser() 
+    config.destdir.mkdir(exist_ok=True, parents=True)
+    print(f"Destination directory: {config.destdir}")
 
-    calibfns = list(cmap_dir.glob(f'*calib*.nc'))
+    #check Calibration Map Dir
+    if isinstance(config.calibmap_dir, str):
+        config.calibmap_dir = Path(config.calibmap_dir)
+    config.calibmap_dir = config.calibmap_dir.expanduser()
+
+    calibfns = list(config.calibmap_dir.glob(f'*calib*.nc'))
     if len(calibfns) < 1:
         raise FileNotFoundError(
-            f"No calibration map files found in {cmap_dir}."
+            f"No calibration map files found in {config.calibmap_dir}."
         )
-    windows = set([fp.stem.split('_')[-1] for fp in calibfns])
-    windows = sorted(list(windows))
 
-    for win in windows:
+    #check windows
+    available_windows = set([fp.stem.split('_')[-1] for fp in calibfns])
+    if config.windows == ['']:
+        valid_windows = available_windows
+    else:
+        valid_windows = [w for w in config.windows if w in available_windows]
+    print(f"Valid windows: {valid_windows}")
+
+    #run conversion
+    for win in valid_windows:
         print(f"Processing window: {win}")
-        calibfns = list(cmap_dir.glob(f'*{win}*.nc'))
+        calibfns = list(config.calibmap_dir.glob(f'*{win}*.nc'))
         if len(calibfns) > 0:
             print(
                 f"Multiple calibration map files found for window {win}. Using the first one: {calibfns[0].name}")
@@ -49,13 +88,11 @@ def main(
 
         calibds = xr.open_dataset(cfn)  # type: ignore
 
-        print(f"Destination directory: {destd}")
-
-        fns = list(datdir.glob(f'**/*{win}*.nc'))
+        fns = list(config.rootdir.glob(f'**/*{win}*.nc'))
         print(f"Found {len(fns)} files to process.")
         fns.sort()
 
-        if photon:
+        if config.photon:
             print("Calibrating to photons/s.cm2.sr...")
             id = 'kp'
             units = 'photons/s.cm2.sr.nm'
@@ -64,8 +101,8 @@ def main(
             id = 'kr'
             units = 'Rayleighs/nm'
 
-        for fn in fns:
-            print(f"Processing file: {fn.name}...", end='', flush=True)
+        for fn in tqdm(fns):
+            # print(f"Processing file: {fn.name}...", end='', flush=True)
             ds = xr.open_dataset(fn)
             ss = ds.copy()
             dwl = np.mean(np.diff(ds.wavelength.data))
@@ -96,9 +133,25 @@ def main(
             encoding = {var: {'zlib': True}
                         for var in (*ss.data_vars.keys(), *ss.coords.keys())}
             print('\tsaving...', end='', flush=True)
-            outfn = destd.joinpath(fn.stem.replace('l1b', 'l1c') + fn.suffix)
+            outfn = config.destdir.joinpath(fn.stem.replace('l1b', 'l1c') + fn.suffix)
             ss.to_netcdf(outfn, encoding=encoding)
             print('\tDone.', flush=True)
 
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Convert L1B files to L1C using calibration maps.")
+    parser.add_argument('--rootdir', type=str, required=True, help="Root directory containing L1B files.")
+    parser.add_argument('--destdir', type=str, default='', help="Destination directory for L1C files. If not provided, will create a 'l1c' directory parallel to the rootdir.")
+    parser.add_argument('--windows', type=str, nargs='*', default=[''], help="List of windows to process. If not provided, will process all windows found in the calibration map directory.")
+    parser.add_argument('--calibmap_dir', type=str, required=True, help="Directory containing calibration map files.")
+    parser.add_argument('--photon',type = bool,required = False, default= True, help="If set, calibrate to photons/s.cm2.sr.nm. If not set, calibrate to Rayleighs/nm.")
+    args = parser.parse_args()
+    l1c_config = L1CConfig(
+        rootdir=args.rootdir,
+        destdir=args.destdir,
+        windows=args.windows,
+        calibmap_dir=args.calibmap_dir,
+        photon=args.photon
+    )
+    main(l1c_config)
 
 # %%
